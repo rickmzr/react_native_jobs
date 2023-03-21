7e9538e649636efd1c64dc1138694ddf3d1cd42d
Rick Mazur <113574185+rickmzr@users.noreply.github.com>
2023-03-21 18:47:28 -0500

    Setup file structure

---
+++ app/index.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+
+import { formToJSON } from "axios";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
